de249bcad080f1a5168f67d0a78ed1d9c7014cfe
Zhukova Alla <90785698+zhuzzzhha@users.noreply.github.com>
2023-05-14 13:18:32 +0300

    Update Modes.js

--- src/components/Modes.js
+++ src/components/Modes.js
@@ -3,31 +3,34 @@ import { useState } from "react";
 import { Button } from "@salutejs/plasma-ui";
 import { Link } from "react-router-dom";
 
-function Modes() {
-  const [selected, setSelected] = useState([]);
 
-  const handleButtonClick = (num) => {
-    if (selected.includes(num)) {
-      setSelected(selected.filter((n) => n !== num));
-    } else {
-      setSelected([...selected, num]);
+export class Modes extends React.Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      note: '',
     }
-  };
+  }
+ 
 
+  render () {
+    const { onModes } = this.props;
   return (
     <div className="btn-group">
       <h1>Выбери режим</h1>
       <div className="divModes">
         <Link to="/learning">
-          <Button onClick={() => handleButtonClick()}>Изучение</Button>
+          <Button onClick= {() => this.props.onModes({note: 'learning'})}>Изучение</Button>
         </Link>
         <p></p>
         <Link to="/repetition">
-          <Button onClick={() => handleButtonClick()}>Повторение</Button>
+          <Button onClick={() => this.props.onModes({note: 'repetition'})}>Повторение</Button>
         </Link>
       </div>
     </div>
   );
-}
 
+  }
+}
 export default Modes;
