eda5fc12dd34cbd1a9027e73278dc54ea5a28493
Svetlana Khlobustova <37548939+SvetlanaKhlobustova50809@users.noreply.github.com>
2023-05-18 22:21:01 +0300

    Update Units.js

--- src/components/Units.js
+++ src/components/Units.js
@@ -1,40 +1,45 @@
-import React from 'react';
-import { useState } from 'react';
-import {Button} from '@salutejs/plasma-ui';
-import { Link } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
+import React from "react";
+import { useState } from "react";
+import { Button } from "@salutejs/plasma-ui";
+import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 export class Units extends React.Component {
-
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      note: '',
-    }
+      note: "",
+    };
   }
- 
 
-  render () {
+  render() {
     const { onUnit } = this.props;
     const { onChangeUn } = this.props;
     console.log("allounit", this.props.onChangeUn.notes[1].title);
     if (this.props.onChangeUn.notes[1].title != "no") {
       console.log(this.props.onChangeUn.notes[1].title);
-      return <Navigate to="/Modes"/>;
+      return <Navigate to="/Modes" />;
     }
-  return (
-    
-    <div className='btn-group2'>
-        <h1>Выбери свой Unit</h1>
-      {[1, 2, 3, 4, 5, 6,7,8,9,10,11,12].map((num) => (
-        <Link to="/Modes">
-            <Button key={num} onClick={() => this.props.onUnit({note: num})}>
-              {num}
-            </Button>
-        </Link>
-      ))}
-    </div>
-  );
-}
+    return (
+      <div>
+        <h1 className="heading">Выбери свой Unit</h1>
+        <Button data-hint="Чтобы начать изучать слова тебе нужно выбрать evolve, unit и нажать start.">
+          Помощь
+        </Button>
+        <div className="btn-group2">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((num) => (
+            <Link to="/Modes">
+              <Button
+                key={num}
+                onClick={() => this.props.onUnit({ note: num })}
+              >
+                {num}
+              </Button>
+            </Link>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 export default Units;
