211e7cace712b053b1001ddb035af69317f19ff3
Svetlana Khlobustova <37548939+SvetlanaKhlobustova50809@users.noreply.github.com>
2023-05-18 22:22:55 +0300

    Update Modes.js

--- src/components/Modes.js
+++ src/components/Modes.js
@@ -2,39 +2,44 @@ import React from "react";
 import { useState } from "react";
 import { Button } from "@salutejs/plasma-ui";
 import { Link } from "react-router-dom";
-import { Navigate } from 'react-router-dom';
-
+import { Navigate } from "react-router-dom";
 
 export class Modes extends React.Component {
-
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      note: '',
-    }
+      note: "",
+    };
   }
- 
 
-  render () {
+  render() {
     const { onChangeMode } = this.props;
     console.log("allomode", this.props.onChangeMode.notes);
-    if (this.props.onChangeMode.notes.length > 2 && this.props.onChangeMode.notes[2].title != "no" && this.props.onChangeMode.notes[2].title != Object) {
-      return (
-      <Navigate to="/learning" />
-      );
+    if (
+      this.props.onChangeMode.notes.length > 2 &&
+      this.props.onChangeMode.notes[2].title != "no" &&
+      this.props.onChangeMode.notes[2].title != Object
+    ) {
+      return <Navigate to="/learning" />;
     }
-  return (
-    <div className="btn-group">
-      <h1>Выбери режим</h1>
-      <div className="divModes">
-        <Link to="/learning">
-          <Button onClick= {() => this.props.onModes('learning')}>Изучение</Button>
-        </Link>
-        <p></p>
+    return (
+      <div>
+        <h1 className="heading">Нажми start</h1>
+        <Button data-hint="Чтобы начать изучать слова тебе нужно выбрать evolve, unit и нажать start.">
+          Помощь
+        </Button>
+        <div className="btn-group">
+          <div className="divModes">
+            <Link to="/learning">
+              <Button onClick={() => this.props.onModes("learning")}>
+                Start
+              </Button>
+            </Link>
+            <p></p>
+          </div>
+        </div>
       </div>
-    </div>
-  );
-
+    );
   }
 }
 export default Modes;
