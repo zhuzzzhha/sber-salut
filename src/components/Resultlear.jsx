7b22d28ccfff9afab9d234041ab8075df333164d
Svetlana Khlobustova <37548939+SvetlanaKhlobustova50809@users.noreply.github.com>
2023-05-18 22:22:26 +0300

    Update Resultlear.jsx

--- src/components/Resultlear.jsx
+++ src/components/Resultlear.jsx
@@ -5,11 +5,18 @@ import { Link } from "react-router-dom";
 
 function Resultlear() {
   return (
-    <div className="outer-div inner-div btn-group">
-      <h1>Поздравляем, ты прошел изучение</h1>
-      <Link to="/">
-        <Button>Выход</Button>
-      </Link>
+    <div>
+      <h1 className="heading">Поздравляем, ты прошел изучение!</h1>
+      <Button data-hint="Чтобы начать изучать слова тебе нужно выбрать evolve, unit и нажать start.">
+          Помощь
+        </Button>
+      <div className="btn-group">
+        <div className="divModes">
+          <Link to="/">
+            <Button>Выход</Button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
