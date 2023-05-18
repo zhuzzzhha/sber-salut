70bf523fa1590adda7de4a96a18c9a96dc6126ed
Svetlana Khlobustova <37548939+SvetlanaKhlobustova50809@users.noreply.github.com>
2023-05-18 22:21:22 +0300

    Update Evolves.js

--- src/components/Evolves.js
+++ src/components/Evolves.js
@@ -1,42 +1,45 @@
-import React from 'react';
-import { useState } from 'react';
-import {Button} from '@salutejs/plasma-ui';
-import { Link } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
+import React from "react";
+import { useState } from "react";
+import { Button } from "@salutejs/plasma-ui";
+import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 export class Evolves extends React.Component {
-
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      note: '',
-    }
+      note: "",
+    };
   }
- 
 
-  render () {
+  render() {
     const { onEvolve } = this.props;
     const { onChangeEv } = this.props;
     console.log("alloevolve", this.props.onChangeEv.notes[0].title);
-   if (this.props.onChangeEv.notes[0].title != "no") {
-      return (
-      <Navigate to="/Unit" />
-      );
+    if (this.props.onChangeEv.notes[0].title != "no") {
+      return <Navigate to="/Unit" />;
     }
-  
-  return (
-    
-    <div className='btn-group'>
-        <h1>Выбери свой Evolve</h1>
-      {[1, 2, 3, 4, 5, 6].map((num) => (
-        <Link to="/Unit">
-            <Button key={num} onClick={() => this.props.onEvolve({note: num})}>
-              {num}
-            </Button>
-        </Link>
-      ))}
-    </div>
-  );
-}
+
+    return (
+      <div>
+        <h1 className="heading">Выбери свой Evolve</h1>
+        <Button data-hint="Чтобы начать изучать слова тебе нужно выбрать evolve, unit и нажать start.">
+          Помощь
+        </Button>
+        <div className="btn-group">
+          {[1, 2, 3, 4, 5, 6].map((num) => (
+            <Link to="/Unit">
+              <Button
+                key={num}
+                onClick={() => this.props.onEvolve({ note: num })}
+              >
+                {num}
+              </Button>
+            </Link>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 export default Evolves;
