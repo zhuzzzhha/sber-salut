e3c068e85338c8557b3fddb46fb6b28d20e4ef3a
Zhukova Alla <90785698+zhuzzzhha@users.noreply.github.com>
2023-05-19 08:32:19 +0300

    Update Modes.js

--- src/components/Modes.js
+++ src/components/Modes.js
@@ -2,19 +2,21 @@ import React from "react";
 import { useState } from "react";
 import { Button } from "@salutejs/plasma-ui";
 import { Link } from "react-router-dom";
-import { Navigate } from "react-router-dom";
+import { Navigate } from 'react-router-dom';
+
 
 export class Modes extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      note: "",
-    };
-  }
 
-  render() {
+  constructor (props) {
+    super(props)
+    this.state = {
+      note: '',
+    }
+  }
+ 
+
+  render () {
     const { onChangeMode } = this.props;
-    console.log("allomode", this.props.onChangeMode.notes);
     if (
       this.props.onChangeMode.notes.length > 2 &&
       this.props.onChangeMode.notes[2].title != "no" &&
@@ -32,7 +34,7 @@ export class Modes extends React.Component {
           <div className="divModes">
             <Link to="/learning">
               <Button onClick={() => this.props.onModes("learning")}>
-                Start
+                Старт
               </Button>
             </Link>
             <p></p>
