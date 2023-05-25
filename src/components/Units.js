78f8524aab7c9e9863bfe05f0c2ed31cbc22b573
Zhukova Alla <90785698+zhuzzzhha@users.noreply.github.com>
2023-05-25 17:01:50 +0300

    Update Units.js

--- src/components/Units.js
+++ src/components/Units.js
@@ -20,17 +20,17 @@ export class Units extends React.Component {
     
     if (this.props.onChangeUn.notes[1].title != "no" && this.props.onChangeUn.notes[0].title != Object) {
       console.log(this.props.onChangeUn.notes[1].title);
-      return <Navigate to="/Modes"/>;
+      return <Navigate to="/learning"/>;
     }
     return (
       <div>
         <h1 className="heading">Выбери свой раздел</h1>
                     <div className="divModes">
-          <Button onClick={() => handleClick()}>Назад</Button>
+          <Button onClick={() => this.handleClick()}>Назад</Button>
         </div>
         <div className="btn-group2">
           {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((num) => (
-            <Link to="/Modes">
+            <Link to="/learning">
               <Button
                 key={num}
                 onClick={() => this.props.onUnit(num )} 
