9688b48690c3f89d45dbd4cdfb06baab7ee927f1
Svetlana Khlobustova <37548939+SvetlanaKhlobustova50809@users.noreply.github.com>
2023-05-29 02:12:52 +0300

    Update Units.js

--- src/components/Units.js
+++ src/components/Units.js
@@ -1,53 +1,44 @@
-import React from 'react';
-import { useState } from 'react';
-import {Button} from '@salutejs/plasma-ui';
-import { Link } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
+import React from "react";
+import { Button } from "@salutejs/plasma-ui";
+import { Navigate } from "react-router-dom";
 
 function handleClick() {
-  window.location.href = '/';
+  window.location.href = "/";
 }
 
 export class Units extends React.Component {
-
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      note: '',
-    }
+      note: "",
+    };
   }
- 
 
-  render () {
-    const { onUnit } = this.props;
-    const { onChangeUn } = this.props;
-    
-    if (this.props.onChangeUn.notes[1].title != "no" && this.props.onChangeUn.notes[0].title != Object) {
+  render() {
+    if (
+      this.props.onChangeUn.notes[1].title !== "no" &&
+      this.props.onChangeUn.notes[0].title !== Object
+    ) {
       console.log(this.props.onChangeUn.notes[1].title);
-      return <Navigate to="/learning"/>;
+      return <Navigate to="/learning" />;
     }
     return (
       <div>
         <h1 className="heading">Выбери свой раздел</h1>
-                    <div className="divModes">
+        <div className="btn-group1">
           <Button onClick={() => handleClick()}>Назад</Button>
         </div>
-        <div className="btn-group2">
+        <div className="btn-group">
           {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((num) => (
-
-              <Button
-                key={num}
-                onClick={() => this.props.onUnit(num )} 
-              >
-                {num}
-              </Button>
-
+            <Button key={num} onClick={() => this.props.onUnit(num)}>
+              {num}
+            </Button>
           ))}
         </div>
-<div class="background">
-    <div class="transparent">Прозрачный текст</div>
- <div class="transparent">Прозрачный текст</div>
-</div>
+        <div class="background">
+          <div class="transparent">Прозрачный текст</div>
+          <div class="transparent">Прозрачный текст</div>
+        </div>
       </div>
     );
   }
