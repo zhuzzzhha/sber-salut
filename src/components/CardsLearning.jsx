928cd7aa8b903cf2ebe7265793bc385e8cf49df9
Svetlana Khlobustova <37548939+SvetlanaKhlobustova50809@users.noreply.github.com>
2023-05-29 02:12:02 +0300

    Update CardsLearning.jsx

--- src/components/CardsLearning.jsx
+++ src/components/CardsLearning.jsx
@@ -3,37 +3,47 @@ import { useState } from "react";
 import { Button, h1 } from "@salutejs/plasma-ui";
 import { Link } from "react-router-dom";
 import ReactCardFlip from "react-card-flip";
-import { Navigate } from 'react-router-dom';
+import { Navigate } from "react-router-dom";
 
-let data = require('./data.json');
-console.log(data)
-  function get_data(evolve, unit){
-    const result = data[evolve][unit];
-    let json_data = []
-    for (let i = 0; i < result.length - 2; i+=2 ) {
-      var newDict = {
-        "title": result[i],
-        "correct": result[i+1]
-      };
-        json_data.push(newDict)
-    }
-    return json_data
+let data = require("./data.json");
+console.log(data);
+function get_data(evolve, unit) {
+  const result = data[evolve][unit];
+  let json_data = [];
+  for (let i = 0; i < result.length - 2; i += 2) {
+    var newDict = {
+      title: result[i],
+      correct: result[i + 1],
+    };
+    json_data.push(newDict);
   }
+  return json_data;
+}
 
-function CardsLearning(props) { 
+function handleClick() {
+  window.location.href = "/unit";
+}
+
+function CardsLearning(props) {
   let evolve = "evolve_1";
   let unit = "unit_1";
   console.log("cardslearning", props.onLearn);
-  if (props.onLearn.notes[0].title != "no" && props.onLearn.notes[0].title!= Object){
-  evolve = "evolve_" + String(props.onLearn.notes[0].title);
+  if (
+    props.onLearn.notes[0].title != "no" &&
+    props.onLearn.notes[0].title != Object
+  ) {
+    evolve = "evolve_" + String(props.onLearn.notes[0].title);
   }
-  if (props.onLearn.notes[1].title != "no"&& props.onLearn.notes[1].title!= Object){
-  unit = "unit_" + String(props.onLearn.notes[1].title);
+  if (
+    props.onLearn.notes[1].title != "no" &&
+    props.onLearn.notes[1].title != Object
+  ) {
+    unit = "unit_" + String(props.onLearn.notes[1].title);
   }
-  
+
   const repetitions = get_data(evolve, unit);
   const [step, setStep] = useState(0);
-  const repetition = repetitions[step]; 
+  const repetition = repetitions[step];
   const len = repetitions.length;
 
   const [flip, setFlip] = useState(false);
@@ -41,47 +51,45 @@ function CardsLearning(props) {
     setFlip(!flip);
     setStep(step + 1);
   };
-  if (props.onLearn.notes.length > 3 && props.onLearn.notes[3].title!= "no" && props.onLearn.notes[3].title!= Object){
-    return (
-      <Navigate to="/Evolve" />
-      );
-    } 
+  if (
+    props.onLearn.notes.length > 3 &&
+    props.onLearn.notes[3].title != "no" &&
+    props.onLearn.notes[3].title != Object
+  ) {
+    return <Navigate to="/Evolve" />;
+  }
+
   return (
-    <div className="divModes">
+    <div>
+      <div className="btn-group1">
+        <Button onClick={() => handleClick()}>Назад</Button>
+      </div>
+      <div className="btn-group2">
         <ReactCardFlip isFlipped={flip} flipDirection="vertical">
           <div className="cardsOff">
             {repetitions[step].title}
             <br />
             <br />
-            <Button className="cardsButton" onClick={() => setFlip(!flip)}>
-              Узнать перевод
-            </Button>
+            <Button onClick={() => setFlip(!flip)}>Узнать перевод</Button>
           </div>
           <div className="cardsOn">
             {repetitions[step].correct}
             <br />
             <br />
-            <Button className="cardsButton" onClick={() => setFlip(!flip)}>
-              Перевернуть
-            </Button>
+            <Button onClick={() => setFlip(!flip)}>Перевернуть</Button>
             <br />
-
             {step == len - 1 ? (
               <Link to="/resultlear">
-                <Button className="cardsButton">Результат</Button>
+                <Button>Результат</Button>
               </Link>
             ) : (
-              <>  
-                <Button
-                  className="cardsButton"
-                  onClick={() => handleButtonClick()}
-                >
-                  Дальше
-                </Button>
+              <>
+                <Button onClick={() => handleButtonClick()}>Дальше</Button>
               </>
             )}
           </div>
         </ReactCardFlip>
+      </div>
     </div>
   );
 }
