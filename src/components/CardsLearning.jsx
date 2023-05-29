446f723a7f6050ae6534dae44b15976b134e7f7f
Zhukova Alla <90785698+zhuzzzhha@users.noreply.github.com>
2023-05-29 21:27:54 +0300

    Update CardsLearning.jsx

--- src/components/CardsLearning.jsx
+++ src/components/CardsLearning.jsx
@@ -6,6 +6,8 @@ import ReactCardFlip from "react-card-flip";
 import { Navigate } from "react-router-dom";
 
 let data = require("./data.json");
+let num_evolve = 1;
+let num_unit = 1;
 console.log(data);
 function get_data(evolve, unit) {
   const result = data[evolve][unit];
@@ -21,7 +23,7 @@ function get_data(evolve, unit) {
 }
 
 function handleClick() {
-  window.location.href = "/";
+  window.location.href = "/unit";
 }
 
 function CardsLearning(props) {
@@ -33,12 +35,14 @@ function CardsLearning(props) {
     props.onLearn.notes[0].title != Object
   ) {
     evolve = "evolve_" + String(props.onLearn.notes[0].title);
+    num_evolve = props.onLearn.notes[0].title;
   }
   if (
     props.onLearn.notes[1].title != "no" &&
     props.onLearn.notes[1].title != Object
   ) {
     unit = "unit_" + String(props.onLearn.notes[1].title);
+    num_unit = props.onLearn.notes[1].title;
   }
 
   const repetitions = get_data(evolve, unit);
@@ -56,14 +60,18 @@ function CardsLearning(props) {
     props.onLearn.notes[3].title != "no" &&
     props.onLearn.notes[3].title != Object
   ) {
-    return <Navigate to="/" />;
+    return <Navigate to="/Evolve" />;
   }
 
   return (
     <div>
       <div className="btn-group1">
-        <Button onClick={() => handleClick()}>К началу</Button>
+        <Button onClick={() => handleClick()}>Назад</Button>
       </div>
+      <div class="rectangle">
+          <span>Уровень: {num_evolve} </span>
+          <span>Раздел: {num_unit} </span>
+        </div>
       <div className="btn-group2">
         <ReactCardFlip isFlipped={flip} flipDirection="vertical">
           <div className="cardsOff">
