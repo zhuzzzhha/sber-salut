24bbfacbe2beebd88aac9829fdd459d8ec5c5501
Svetlana Khlobustova <37548939+SvetlanaKhlobustova50809@users.noreply.github.com>
2023-05-31 13:41:08 +0300

    Update Units.js

--- src/components/Units.js
+++ src/components/Units.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Button } from "@salutejs/plasma-ui";
 import { Navigate } from "react-router-dom";
 
+let num_evolve = 1;
+
 function handleClick() {
   window.location.href = "/";
 }
@@ -15,28 +17,24 @@ export class Units extends React.Component {
   }
 
   render() {
-    let unit = 1;
-    if (this.props.onChangeUn.notes[0].title !== "no")
-    {
-      unit = this.props.onChangeUn.notes[0].title;
-    }
     if (
       this.props.onChangeUn.notes[1].title !== "no" &&
       this.props.onChangeUn.notes[0].title !== Object
-      
     ) {
-      
       console.log(this.props.onChangeUn.notes[1].title);
+      num_evolve = this.props.onChangeUn.notes[0].title;
       return <Navigate to="/learning" />;
     }
+
     return (
       <div>
         <h1 className="heading">Выбери свой раздел</h1>
         <div className="btn-group1">
           <Button onClick={() => handleClick()}>Назад</Button>
         </div>
-        <div class="rectangle">
-          <span>Уровень: {unit} </span>
+        <br />
+        <div class="heading">
+          <span>Уровень: {num_evolve} </span>
         </div>
         <div className="btn-group">
           {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((num) => (
@@ -49,7 +47,6 @@ export class Units extends React.Component {
           <div class="transparent">Прозрачный текст</div>
           <div class="transparent">Прозрачный текст</div>
         </div>
-
       </div>
     );
   }
