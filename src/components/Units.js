529e7b12d14d86c5a323e518233b41e1400229b1
Zhukova Alla <90785698+zhuzzzhha@users.noreply.github.com>
2023-06-15 15:44:53 +0300

    Update Units.js

--- src/components/Units.js
+++ src/components/Units.js
@@ -1,57 +1,57 @@
-import React from "react";
-import { Button } from "@salutejs/plasma-ui";
-import { Navigate } from "react-router-dom";
+import React, {useEffect} from "react";
+import {Button} from "@salutejs/plasma-ui";
+// import { Button } from '@mui/material';
+import { useParams} from "react-router-dom";
 
-function handleClick() {
-  window.location.href = "/";
-}
-export class Units extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      note: "",
-    };
-  }
+let evolve = 1;
 
-  render() {
-    let unit = 1;
-    if (this.props.onChangeUn.notes[0].title !== "no")
-    {
-      unit = this.props.onChangeUn.notes[0].title;
-    }
-    if (
-      this.props.onChangeUn.notes[1].title !== "no" &&
-      this.props.onChangeUn.notes[0].title !== Object
+// function handleClickBack() {
+//   window.location.href = "/";
+// }
 
-    ) {
+function Units(props) {
+  const {evolve} = useParams();
+  const {onOpen, onChoose, onBack} = props;
 
-      console.log(this.props.onChangeUn.notes[1].title);
-      return <Navigate to="/learning" />;
-    }
-    return (
-      <div>
-        <h1 className="heading">Выбери свой раздел</h1>
-        <div className="btn-group1">
-          <Button onClick={() => handleClick()}>Назад</Button>
-        </div>
-          <br />
-        <div class="heading">
-          <span>Уровень: {unit} </span>
-        </div>
-        <div className="btn-group">
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((num) => (
-            <Button key={num} onClick={() => this.props.onUnit(num)}>
-              {num}
-            </Button>
-          ))}
-        </div>
-        <div class="background">
-          <div class="transparent">Прозрачный текст</div>
-          <div class="transparent">Прозрачный текст</div>
-        </div>
+  useEffect(() => {
+    console.log("Unit: useEffect");
+    onOpen({ evolve });
+    return () => {};
+  }, []);
 
+  return (
+    <div>
+      <h1 className="heading">Выбери свой раздел</h1>
+
+      <div className="btn-group1">
+        <Button
+          onClick={() => onBack({evolve})}
+        >Назад</Button>
       </div>
-    );
-  }
+
+      <br/>
+
+      <div className="heading">
+        <span>Уровень: {evolve} </span>
+      </div>
+
+      <div className="btn-group">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((unit) => (
+          <Button
+            onClick={() => onChoose({evolve, unit})}
+            key={unit}
+          >
+            {unit}
+          </Button>
+        ))}
+      </div>
+      <div className="background">
+        <div className="transparent">Прозрачный текст</div>
+        <div className="transparent">Прозрачный текст</div>
+      </div>
+    </div>
+  );
+  // }
 }
+
 export default Units;
